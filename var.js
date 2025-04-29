845a3cfc33b8da8a65448721ba1cca3a91675bdf
Shubhhya17 <shubhpawar1703@gmail.com>
2025-04-29 22:28:39 +0530

    typeScript_first_code

---
+++ var.js
@@ -0,0 +1,5 @@
+var sub = "Shubham";
+var msg = "Hello ".concat(sub, " how are you?"); // ✅ fixed here
+console.log(msg);
+var decimal = 6;
+console.log(decimal);
---
+++ var.ts
@@ -0,0 +1,6 @@
+var sub: string = "Shubham";
+var msg: string = `Hello ${sub} how are you?`; // ✅ fixed here
+console.log(msg);
+
+var decimal: number = 6;
+console.log(decimal);
